d5fb71de83e23e15c843bf9eb598c95adbdf76cb
sales-edel <sales.edelwella@gmail.com>
2023-09-14 15:18:27 +0300

    zmina big

--- js/order.js
+++ js/order.js
@@ -10,17 +10,20 @@
          try {
              var r = JSON.parse(xhr.responseText),
                 result = r["result"];
-                output = `<p>${r["result"][0][0]}</p>`
-                output += `<table>`
+                /*  output = `<p>${r["result"][0][0]}</p>`  */
+              /*   output += `<table>` */
              for (var i = 1; i < result.length; i++){
-                   output += `<tr>`
-                   output += `<th>${r["result"][i][0]}</th>`
-                   output += `<th>${r["result"][i][2]}</th>`
-                   output += `<th>${r["result"][i][5]}</th>`
-                   output += `</tr>`
+                  output += `<div class="order">` 
+                  /*  output += `<div>${r["result"][i][0]}</div>` */
+                   output += `<div>${r["result"][i][1]}</div>`
+                   output += `<img src="./image/${r["result"][i][4]}" alt="Velo-Retro" width="300px">`
+                  /*  <img src="image/retro_1.png" alt="Velo-Retro" width="300px"> */
+                   output += `<div>${r["result"][i][2]}</div>`
+                   output += `<div class="order_number"> Ціна: ${r["result"][i][3].toFixed(0)} грн</div>`
+                   output += `</div>`
                   //  output += join("<br/>") + "<br/><hr/>";
              }
-             output += `</table>`
+             /* output += `</table>` */
          } catch(e) {}
       } 
       
